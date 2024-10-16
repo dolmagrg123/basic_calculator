dc857174d002eec0c9ed724578d1bd17bfef21b9
Dolma <dolmatgurung1@gmail.com>
2024-10-15 21:11:09 -0400

    retrieve user input testing

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    # Retrieve and convert user inputs to float
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']  # Get the selected operation
+    # calculator.add(num1, num2) #testing
+    print(num1 + num2)
+
     return render_template('index.html')
 
 if __name__ == "__main__":
